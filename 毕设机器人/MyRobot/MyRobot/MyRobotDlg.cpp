01619873c5048828a71e0e96a1d05c4a2d9f6eae
wqq <1258552199@qq.com>
2018-04-12 13:13:14 +0800

    Test

--- 毕设机器人/MyRobot/MyRobot/MyRobotDlg.cpp
+++ 毕设机器人/MyRobot/MyRobot/MyRobotDlg.cpp
@@ -225,8 +225,8 @@ void CMyRobotDlg::OnBnClickedServoOn()
 void CMyRobotDlg::OnBnClickedButtonHome()
 {
 	// TODO:  在此添加控件通知处理程序代码
-	CWaitCursor wc;
-	Robot->Home();
+//	CWaitCursor wc;
+//	Robot->Home();
 	for (int i = 0; i<Robot->m_JointNumber; i++)
 		Robot->m_JointArray[i].LastJointPosition = 0;
 }
