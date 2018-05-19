bab9fe096dafd315126500d9cc3f7b9d7baea9be
wqq <1258552199@qq.com>
2018-05-19 19:47:46 +0800

    测试是不是CPU太差

--- 毕设机器人/ShowRobotData/ShowRobotData/ShowRobotDataDlg.cpp
+++ 毕设机器人/ShowRobotData/ShowRobotData/ShowRobotDataDlg.cpp
@@ -13,7 +13,7 @@
 #endif
 
 // 用于应用程序“关于”菜单项的 CAboutDlg 对话框
-
+#define UM_DRAWROBOTDATA    WM_USER+100    //自定义一个消息
 UINT server_thd(LPVOID p);   //申明线程函数
 CString IP;  //定义为全局变量
 SOCKET listen_sock;
@@ -76,6 +76,7 @@ BEGIN_MESSAGE_MAP(CShowRobotDataDlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 	ON_BN_CLICKED(IDC_STARTSERVER, &CShowRobotDataDlg::OnBnClickedStartserver)
 	ON_LBN_SELCHANGE(IDC_LIST1_SHOWMESSAGE, &CShowRobotDataDlg::OnLbnSelchangeList1Showmessage)
+	ON_MESSAGE(UM_DRAWROBOTDATA, &CShowRobotDataDlg::OnDrawRobotData)
 END_MESSAGE_MAP()
 
 
@@ -174,10 +175,13 @@ BOOL CShowRobotDataDlg::OnInitDialog()
 	pLabel = m_ChartCtrl4.GetLeftAxis()->GetLabel();
 	pLabel->SetText(str1);
 
-	m_pLineSerie1 = m_ChartCtrl1.CreateLineSerie();
-	m_pLineSerie2 = m_ChartCtrl1.CreateLineSerie();
-	m_pLineSerie3 = m_ChartCtrl1.CreateLineSerie();
-
+	for (int i = 0; i < 3; i++)
+	{
+		m_pLineSerie1[i] = m_ChartCtrl1.CreateLineSerie();
+		m_pLineSerie2[i] = m_ChartCtrl2.CreateLineSerie();
+		m_pLineSerie3[i] = m_ChartCtrl3.CreateLineSerie();
+		m_pLineSerie4[i] = m_ChartCtrl4.CreateLineSerie();
+	}
 	/////////////////////////////////////////////////
 
 
@@ -272,7 +276,56 @@ void CShowRobotDataDlg::OnBnClickedStartserver()
 	
 	update(_T("本服务器IP地址：") + IP);
 
-	AfxBeginThread(server_thd, NULL);//创建线程
+	/////////////////////下面是画图程序的初始化
+	for (int i = 0; i < 3; i++)
+	{
+		ZeroMemory(&m_HightSpeedChartArray1[i], sizeof(double)*m_c_arrayLength);
+		ZeroMemory(&m_HightSpeedChartArray2[i], sizeof(double)*m_c_arrayLength);
+		ZeroMemory(&m_HightSpeedChartArray3[i], sizeof(double)*m_c_arrayLength);
+		ZeroMemory(&m_HightSpeedChartArray4[i], sizeof(double)*m_c_arrayLength);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		m_pLineSerie1[i]->SetSeriesOrdering(poNoOrdering);
+		m_pLineSerie2[i]->SetSeriesOrdering(poNoOrdering);
+		m_pLineSerie3[i]->SetSeriesOrdering(poNoOrdering);
+		m_pLineSerie4[i]->SetSeriesOrdering(poNoOrdering);
+	}
+	for (size_t i = 0; i<m_c_arrayLength; ++i)
+	{
+		m_X[i] = i;   //X轴的参数初始化
+	}
+	m_count = m_c_arrayLength;
+	for (int i = 0; i < 3; i++)
+	{
+		m_pLineSerie1[i]->ClearSerie();
+		m_pLineSerie2[i]->ClearSerie();
+		m_pLineSerie3[i]->ClearSerie();
+		m_pLineSerie4[i]->ClearSerie();
+	}
+	TChartStringStream strs1, strs2, strs3;
+	strs1 << _T("角度值");
+		m_pLineSerie1[0]->SetName(strs1.str());//SetName的作用将在后面讲到
+		m_pLineSerie2[0]->SetName(strs1.str());
+		m_pLineSerie3[0]->SetName(strs1.str());
+		m_pLineSerie4[0]->SetName(strs1.str());
+	strs2 << _T("速度值");
+		m_pLineSerie1[1]->SetName(strs2.str());//SetName的作用将在后面讲到
+		m_pLineSerie2[1]->SetName(strs2.str());
+		m_pLineSerie3[1]->SetName(strs2.str());
+		m_pLineSerie4[1]->SetName(strs2.str());
+	strs3 << _T("力矩");
+		m_pLineSerie1[2]->SetName(strs3.str());//SetName的作用将在后面讲到
+		m_pLineSerie2[2]->SetName(strs3.str());
+		m_pLineSerie3[2]->SetName(strs3.str());
+		m_pLineSerie4[2]->SetName(strs3.str());
+    m_ChartCtrl1.GetLegend()->SetVisible(true);
+	m_ChartCtrl2.GetLegend()->SetVisible(true);
+	m_ChartCtrl3.GetLegend()->SetVisible(true);
+	m_ChartCtrl4.GetLegend()->SetVisible(true);
+	///////////////////画图程序的初始化结束
+	AfxBeginThread(server_thd, (LPVOID)GetSafeHwnd());//创建线程
 }
 
 
@@ -287,15 +340,26 @@ void CShowRobotDataDlg::update(CString s)
 	m_recvMessage.AddString(s);
 }
 
-
+/// \brief 左移数组  
+/// \param ptr 数组指针  
+/// \param data 新数值  
+/// 
+void LeftMoveArray(double* ptr, size_t length, double data)
+{
+	for (size_t i = 1; i<length; ++i)
+	{
+		ptr[i - 1] = ptr[i];
+	}
+	ptr[length - 1] = data;
+}
 
 UINT server_thd(LPVOID p)//线程要调用的函数
 {
+	HWND hWnd = (HWND)p;
 	SOCKADDR_IN local_addr;
 	SOCKADDR_IN client_addr;
 	int iaddrSize = sizeof(SOCKADDR_IN);
 	int res;
-	char msg[1024];
 	CShowRobotDataDlg * dlg = (CShowRobotDataDlg *)AfxGetApp()->GetMainWnd();
 	char ch_ip[20];
 	CString2Char(IP, ch_ip);//注意！这里调用了字符格式转换函数，此函数功能：CString类型转换为Char类型，实现代码在后面添加
@@ -325,22 +389,143 @@ UINT server_thd(LPVOID p)//线程要调用的函数
 		port.Format(_T("%d"), int(ntohs(client_addr.sin_port)));
 		dlg->update(_T("已连接客户端：") + CString(inet_ntoa(client_addr.sin_addr)) + "  端口：" + port);
 	}
+
+
 	////////////接收数据
+
+	RobotData MyRobotData;
+	char recbuf[sizeof(MyRobotData)];
+	memset(recbuf, 0, sizeof(MyRobotData));
 	while (1)
 	{		
-		if ((res = recv(sock, msg, 1024, 0)) == -1)    //这个rev函数也是阻塞模式
+		if ((res = recv(sock, recbuf, sizeof(recbuf), 0)) == -1)    //这个rev函数也是阻塞模式
 		{
 			dlg->update(_T("失去客户端的连接"));
 			break;
 		}
 		else
 		{
-		//	msg[res] = '\0';
-			dlg->update(_T("client:") + CString(msg));
+			memset(&MyRobotData, 0, sizeof(MyRobotData));
+			memcpy(&MyRobotData, recbuf, sizeof(MyRobotData));
+			::PostMessageA(hWnd,UM_DRAWROBOTDATA,(WPARAM)&MyRobotData,1);
+			//dlg->DrawData(MyRobotData);
 		}
+
 
 	}
 	WSACleanup();
 	return 0;
 }
 
+LRESULT CShowRobotDataDlg::OnDrawRobotData(WPARAM wParam, LPARAM lParam)
+{
+	RobotData *myRobotData;
+	myRobotData = (RobotData*)wParam;
+	++m_count;
+	m_pLineSerie1[0]->ClearSerie();
+	m_pLineSerie1[1]->ClearSerie();
+	m_pLineSerie1[2]->ClearSerie();
+
+	m_pLineSerie2[0]->ClearSerie();
+	m_pLineSerie2[1]->ClearSerie();
+	m_pLineSerie2[2]->ClearSerie();
+
+	m_pLineSerie3[0]->ClearSerie();
+	m_pLineSerie3[1]->ClearSerie();
+	m_pLineSerie3[2]->ClearSerie();
+
+	m_pLineSerie4[0]->ClearSerie();
+	m_pLineSerie4[1]->ClearSerie();
+	m_pLineSerie4[2]->ClearSerie();
+
+	LeftMoveArray(m_HightSpeedChartArray1[0], m_c_arrayLength, myRobotData->JointsNow[0]);  //角度值
+	LeftMoveArray(m_HightSpeedChartArray1[1], m_c_arrayLength, myRobotData->JointsVelNow[0]);  //速度值
+	LeftMoveArray(m_HightSpeedChartArray1[2], m_c_arrayLength, myRobotData->JointsNow[0]);  //力矩
+
+	LeftMoveArray(m_HightSpeedChartArray2[0], m_c_arrayLength, myRobotData->JointsNow[1]);  //角度值
+	LeftMoveArray(m_HightSpeedChartArray2[1], m_c_arrayLength, myRobotData->JointsVelNow[1]);  //速度值
+	LeftMoveArray(m_HightSpeedChartArray2[2], m_c_arrayLength, myRobotData->JointsNow[1]);  //力矩
+
+	LeftMoveArray(m_HightSpeedChartArray3[0], m_c_arrayLength, myRobotData->JointsNow[2]);  //角度值
+	LeftMoveArray(m_HightSpeedChartArray3[1], m_c_arrayLength, myRobotData->JointsVelNow[2]);  //速度值
+	LeftMoveArray(m_HightSpeedChartArray3[2], m_c_arrayLength, myRobotData->JointsNow[2]);  //力矩
+
+	LeftMoveArray(m_HightSpeedChartArray4[0], m_c_arrayLength, myRobotData->JointsNow[3]);  //角度值
+	LeftMoveArray(m_HightSpeedChartArray4[1], m_c_arrayLength, myRobotData->JointsVelNow[3]);  //速度值
+	LeftMoveArray(m_HightSpeedChartArray4[2], m_c_arrayLength, myRobotData->JointsNow[3]);  //力矩
+
+	LeftMoveArray(m_X, m_c_arrayLength, m_count);
+
+	m_pLineSerie1[0]->AddPoints(m_X, m_HightSpeedChartArray1[0], m_c_arrayLength);
+	m_pLineSerie1[1]->AddPoints(m_X, m_HightSpeedChartArray1[1], m_c_arrayLength);
+	m_pLineSerie1[2]->AddPoints(m_X, m_HightSpeedChartArray1[2], m_c_arrayLength);
+
+	m_pLineSerie2[0]->AddPoints(m_X, m_HightSpeedChartArray2[0], m_c_arrayLength);
+	m_pLineSerie2[1]->AddPoints(m_X, m_HightSpeedChartArray2[1], m_c_arrayLength);
+	m_pLineSerie2[2]->AddPoints(m_X, m_HightSpeedChartArray2[2], m_c_arrayLength);
+
+	m_pLineSerie3[0]->AddPoints(m_X, m_HightSpeedChartArray3[0], m_c_arrayLength);
+	m_pLineSerie3[1]->AddPoints(m_X, m_HightSpeedChartArray3[1], m_c_arrayLength);
+	m_pLineSerie3[2]->AddPoints(m_X, m_HightSpeedChartArray3[2], m_c_arrayLength);
+
+	m_pLineSerie4[0]->AddPoints(m_X, m_HightSpeedChartArray4[0], m_c_arrayLength);
+	m_pLineSerie4[1]->AddPoints(m_X, m_HightSpeedChartArray4[1], m_c_arrayLength);
+	m_pLineSerie4[2]->AddPoints(m_X, m_HightSpeedChartArray4[2], m_c_arrayLength);
+
+	return 1;
+}
+
+void CShowRobotDataDlg::DrawData(RobotData myrobotdata)
+{
+	++m_count;
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	m_pLineSerie1[i]->ClearSerie();
+	//	m_pLineSerie2[i]->ClearSerie();
+	//	m_pLineSerie3[i]->ClearSerie();
+	//	m_pLineSerie4[i]->ClearSerie();
+	//}
+	m_pLineSerie1[0]->ClearSerie();
+	m_pLineSerie1[1]->ClearSerie();
+	m_pLineSerie1[2]->ClearSerie();
+
+	LeftMoveArray(m_HightSpeedChartArray1[0], m_c_arrayLength, myrobotdata.JointsNow[0]);  //角度值
+	LeftMoveArray(m_HightSpeedChartArray1[1], m_c_arrayLength, myrobotdata.JointsVelNow[0]);  //速度值
+	LeftMoveArray(m_HightSpeedChartArray1[2], m_c_arrayLength, myrobotdata.JointsNow[0]);  //力矩
+
+	LeftMoveArray(m_X, m_c_arrayLength, m_count);
+
+	m_pLineSerie1[0]->AddPoints(m_X, m_HightSpeedChartArray1[0], m_c_arrayLength);
+	m_pLineSerie1[1]->AddPoints(m_X, m_HightSpeedChartArray1[1], m_c_arrayLength);
+	m_pLineSerie1[2]->AddPoints(m_X, m_HightSpeedChartArray1[2], m_c_arrayLength);
+
+	//LeftMoveArray(m_HightSpeedChartArray2[0], m_c_arrayLength, myrobotdata.JointsNow[1]);  //角度值
+	//LeftMoveArray(m_HightSpeedChartArray2[1], m_c_arrayLength, myrobotdata.JointsVelNow[1]);  //速度值
+	//LeftMoveArray(m_HightSpeedChartArray2[2], m_c_arrayLength, myrobotdata.JointsNow[1]);  //力矩
+
+	//LeftMoveArray(m_HightSpeedChartArray3[0], m_c_arrayLength, myrobotdata.JointsNow[2]);  //角度值
+	//LeftMoveArray(m_HightSpeedChartArray3[1], m_c_arrayLength, myrobotdata.JointsVelNow[2]);  //速度值
+	//LeftMoveArray(m_HightSpeedChartArray3[2], m_c_arrayLength, myrobotdata.JointsNow[2]);  //力矩
+
+	//LeftMoveArray(m_HightSpeedChartArray4[0], m_c_arrayLength, myrobotdata.JointsNow[3]);  //角度值
+	//LeftMoveArray(m_HightSpeedChartArray4[1], m_c_arrayLength, myrobotdata.JointsVelNow[3]);  //速度值
+	//LeftMoveArray(m_HightSpeedChartArray4[2], m_c_arrayLength, myrobotdata.JointsNow[3]);  //力矩
+
+	LeftMoveArray(m_X, m_c_arrayLength, m_count);
+
+	m_pLineSerie1[0]->AddPoints(m_X, m_HightSpeedChartArray1[0], m_c_arrayLength);
+	m_pLineSerie1[1]->AddPoints(m_X, m_HightSpeedChartArray1[1], m_c_arrayLength);
+	m_pLineSerie1[2]->AddPoints(m_X, m_HightSpeedChartArray1[2], m_c_arrayLength);
+
+	//m_pLineSerie2[0]->AddPoints(m_X, m_HightSpeedChartArray2[0], m_c_arrayLength);
+	//m_pLineSerie2[1]->AddPoints(m_X, m_HightSpeedChartArray2[1], m_c_arrayLength);
+	//m_pLineSerie2[2]->AddPoints(m_X, m_HightSpeedChartArray2[2], m_c_arrayLength);
+
+	//m_pLineSerie3[0]->AddPoints(m_X, m_HightSpeedChartArray3[0], m_c_arrayLength);
+	//m_pLineSerie3[1]->AddPoints(m_X, m_HightSpeedChartArray3[1], m_c_arrayLength);
+	//m_pLineSerie3[2]->AddPoints(m_X, m_HightSpeedChartArray3[2], m_c_arrayLength);
+
+	//m_pLineSerie4[0]->AddPoints(m_X, m_HightSpeedChartArray4[0], m_c_arrayLength);
+	//m_pLineSerie4[1]->AddPoints(m_X, m_HightSpeedChartArray4[1], m_c_arrayLength);
+	//m_pLineSerie4[2]->AddPoints(m_X, m_HightSpeedChartArray4[2], m_c_arrayLength);
+}
--- 毕设机器人/ShowRobotData/ShowRobotData/ShowRobotDataDlg.h
+++ 毕设机器人/ShowRobotData/ShowRobotData/ShowRobotDataDlg.h
@@ -11,7 +11,19 @@
 #include "ChartCtrl/ChartAxis.h"
 #include "afxcmn.h"
 
-
+struct RobotData
+{
+	double JointsNow[4];
+	double JointsNext[4];
+	double JointsVelNow[4];
+	double JointsVelNext[4];
+	double Origin6axisForce[6];
+	double JointsTorque[4];
+	double CartesianPositionNow[4];
+	double CartesianPositionNext[4];
+	double CartesianVelNow[4];
+	double CartesianVelNext[4];
+};
 
 // CShowRobotDataDlg 对话框
 class CShowRobotDataDlg : public CDialogEx
@@ -44,13 +56,24 @@ public:
 	CChartCtrl m_ChartCtrl3;
 	CChartCtrl m_ChartCtrl4;
 
-	CChartLineSerie* m_pLineSerie1;
-	CChartLineSerie* m_pLineSerie2;
-	CChartLineSerie* m_pLineSerie3;
+	CChartLineSerie* m_pLineSerie1[3];
+	CChartLineSerie* m_pLineSerie2[3];
+	CChartLineSerie* m_pLineSerie3[3];
+	CChartLineSerie* m_pLineSerie4[3];
+
+	double m_HightSpeedChartArray1[3][2096];  //Y轴参数
+	double m_HightSpeedChartArray2[3][2096];
+	double m_HightSpeedChartArray3[3][2096];
+	double m_HightSpeedChartArray4[3][2096];
+	double m_X[2096];   //X轴参数
+	unsigned int m_count;
+	const size_t m_c_arrayLength=500;
 	afx_msg void OnBnClickedStartserver();
+
 	// //本程序作为服务器接受的主机的消息
 	CListBox m_recvMessage;
 	afx_msg void OnLbnSelchangeList1Showmessage();
+	afx_msg LRESULT OnDrawRobotData(WPARAM WParam, LPARAM LParam);
 
 public:
 	void update(CString s); 	
@@ -61,6 +84,7 @@ private: CEdit* send_edit;
 public:
 	CEdit m_TCPPort;
 	CIPAddressCtrl m_TCPIPaddr;
+	void DrawData(RobotData myrobotdata);
 };
 
 
