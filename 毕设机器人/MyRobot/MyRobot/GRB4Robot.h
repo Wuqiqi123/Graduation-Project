9cc4f1c8e14a72a11e316cd1a9428a0be14a3802
wqq <1258552199@qq.com>
2019-03-14 20:00:21 +0800

    用于拍视频的版本，使用后三根轴，第一根轴没有用

--- 毕设机器人/MyRobot/MyRobot/GRB4Robot.h
+++ 毕设机器人/MyRobot/MyRobot/GRB4Robot.h
@@ -10,9 +10,9 @@ const unsigned short OpenMask[16] =
 	0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080,
 	0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000
 };
-#define l1   250			
-#define l2   150
-#define l3   120
+#define l1   (250*0.001)		
+#define l2   (150*0.001)
+#define l3   (120*0.001)
 //#define l3   4	
 class CGRB4Robot :
 	public CRobotBase
